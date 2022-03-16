dfcc07f80118d61f3268874ee4d0c39293f8d1d2
Atanu Biswas <atanubiswas484@gmail.com>
2022-03-16 21:31:19 +0200

    stringE

--- stringE/stringE.java
+++ stringE/stringE.java
@@ -1,2 +1,20 @@
-package stringE;public class stringE {
+package stringE;
+
+public class stringE {
+    public static boolean stringE(String str) {
+        int count = 0;
+
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == 'e') count++;
+            // alternately: str.substring(i, i+1).equals("e")
+        }
+
+        return (count >= 1 && count <= 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringE("Hello"));
+        System.out.println(stringE("Heello"));
+        System.out.println(stringE("Heeoeelo"));
+    }
 }
