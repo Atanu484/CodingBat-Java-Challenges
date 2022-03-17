01547a6d54062a1160420da35e847da8e90e1aae
Atanu Biswas <atanubiswas484@gmail.com>
2022-03-17 23:23:24 +0200

    frontBack.java

--- frontBack/frontBack.java
+++ frontBack/frontBack.java
@@ -1,2 +1,15 @@
-package frontBack;public class frontBack {
+package frontBack;
+
+public class frontBack {
+    public static String frontBack(String str) {
+        if (str.length() <= 1) return str;
+        String mid = str.substring(1, str.length()-1);
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(frontBack("code"));
+        System.out.println(frontBack("a"));
+        System.out.println(frontBack("abcd"));
+    }
 }
