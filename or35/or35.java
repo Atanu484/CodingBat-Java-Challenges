a330616c35f068fb83467c68002e990ee345627c
Atanu Biswas <atanubiswas484@gmail.com>
2022-03-18 12:43:18 +0200

    or35

--- or35/or35.java
+++ or35/or35.java
@@ -1,2 +1,16 @@
-package or35;public class or35 {
+package or35;
+
+public class or35 {
+    public static boolean or35(int n) {
+        if(n > 0 && (n % 3 == 0 || n % 5 == 0)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(or35(3));
+        System.out.println(or35(8));
+        System.out.println(or35(10));
+    }
 }
