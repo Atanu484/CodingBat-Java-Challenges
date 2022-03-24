bde1b14b1936aae07a9c99e001ada820d1f219a3
Atanu Biswas <atanubiswas484@gmail.com>
2022-03-24 23:41:03 +0200

    makes10

--- makes10/makes10.java
+++ makes10/makes10.java
@@ -1,2 +1,18 @@
-package makes10;public class makes10 {
+package makes10;
+
+public class makes10 {
+    public static boolean makes10(int a, int b) {
+        if(a == 10 || b == 10){
+            return true;
+        }else if(a + b == 10){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makes10(9,10));
+        System.out.println(makes10(9,9));
+        System.out.println(makes10(1,9));
+    }
 }
